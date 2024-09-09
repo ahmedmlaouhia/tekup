ff59fcbcd30a20ba56b75d36a4f1da6a540efa22
ahmedmlaouhia <ahmedmlaouhia6@gmail.com>
2024-09-09 16:16:53 +0100

    init commit

---
+++ react/tp1/src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css"
+import Features from "./components/Features"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Features />
+      <Footer />
+    </>
+  )
+}
+
+export default App
